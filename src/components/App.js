0e2e1486e5c1a67a6f8932176052d7cbeca48309
yyokii <yyokii.h@gmail.com>
2018-06-24 00:20:43 +0900

    ファイル構成を修正

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: {
+        firstWord: 'Infinity',
+        secondWord: 'Idea',
+      },
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +24,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>{this.state.words.firstWord}</div>
+        <div>{this.state.words.secondWord}</div>
+        <div></div>
       </div>
     );
   }
